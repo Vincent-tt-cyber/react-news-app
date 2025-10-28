afb9f0e2a83b84619caad1f43ccbeb88e587b0b2
Vincent-tt-cyber <evgeniy161102@gmail.com>
2025-10-28 16:34:13 +0300

    Изменена логика запросов к API

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,7 @@ import Header from "./components/Header/Header";
 import Main from "./pages/Main/Main";
 
 /* 
-TODO: 
-
-Делать по видео: https://youtu.be/3iE8BbL0038?list=PLu_vAIOaYQ6Q5yLM3gtHmrrtYE-MywJg2&t=109
-Подождать окончание лимита API
+TODO: https://youtu.be/suowP9mWAso?list=PLu_vAIOaYQ6Q5yLM3gtHmrrtYE-MywJg2&t=417
 
 */
 
